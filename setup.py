ec0f18a0c6a0b3d81495ecd13af550d893a204a6
Ezra Peisach <ezra.peisach@rcsb.org>
2019-01-06 15:12:22 -0500

    classifiers in setup.py should be a list and not a tuple

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     url='https://github.com/rcsb/py-wwpdb_utils_seqdb_v2',
     #
     license='Apache 2.0',
-    classifiers=(
+    classifiers=[
         'Development Status :: 3 - Alpha',
         # 'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -39,7 +39,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-    ),
+    ],
     #
     install_requires=['mmcif', 'mmcif.utils', 'wwpdb.utils.config'],
     packages=find_packages(exclude=['wwpdb.utils.tests-seqdb', 'mock-data', 'tests.*']),
