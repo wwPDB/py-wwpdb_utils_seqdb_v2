f3522f34969d9965d785237e85333af796b20dd2
Ezra Peisach <ezra.peisach@rcsb.org>
2020-09-21 14:12:08 -0400

    Use a local uniprot variant splice file to reinstate FastaUtilTests

--- wwpdb/utils/tests-seqdb/FastaUtilTests.py
+++ wwpdb/utils/tests-seqdb/FastaUtilTests.py
@@ -18,23 +18,19 @@ import os.path
 import string
 import traceback
 
-from wwpdb.utils.config.ConfigInfo import ConfigInfo, getSiteId
 from wwpdb.utils.seqdb_v2.FastaUtil import FastaUtil
 
 
-@unittest.skip("Cannot test without sequence fasta files")
 class FastaUtilTests(unittest.TestCase):
     def setUp(self):
         self.__verbose = True
         self.__lfh = sys.stderr
-        # Pick up site information from the environment or failover to the development site id.
-        self.__siteId = getSiteId(defaultSiteId="WWPDB_DEPLOY_TEST")
-        self.__lfh.write("\nTesting with site environment for:  %s\n" % self.__siteId)
         #
-        cI = ConfigInfo(self.__siteId)
-        self.__fastaPath = cI.get("SITE_REFDATA_SEQUENCE_DB_PATH")
-        self.__variantFastaFilePath = os.path.join(self.__fastaPath, "uniprot_sprot_varsplic.fasta")
-        self.__unpIdListV = ["P42284", "P42284-1", "P42284-3", "P29994-1", "P29994-2", "P29994-3", "P29994-4", "P29994-5", "P29994-6", "P29994-7"]
+        here = os.path.abspath(os.path.dirname(__file__))
+        fastaPath = os.path.join(here, "refdata")
+        self.__variantFastaFilePath = os.path.join(fastaPath, "uniprot_sprot_varsplic.fasta")
+        self.__unpIdListV = ["P42284-1", "P42284-3", "P29994-2", "P29994-3", "P29994-4", "P29994-5", "P29994-6", "P29994-7"]
+        self.__unpIdNotV = ["P42284"]
 
     def tearDown(self):
         pass
@@ -68,6 +64,15 @@ class FastaUtilTests(unittest.TestCase):
                         )
                     )
                     self.__lfh.write("%s\n" % self.__cleanString(sD[uid]["sequence"]))
+                else:
+                    self.__lfh.write("id %s not found\n" % uid)
+                    self.fail()
+
+            # Test for entries that should not be there
+            for uid in self.__unpIdNotV:
+                if uid in sD:
+                    self.__lfh.write("id %s found in variant file when it should not\n" % uid)
+                    self.fail()
 
         except:  # noqa: E722 pylint: disable=bare-except
             traceback.print_exc(file=self.__lfh)
@@ -82,7 +87,6 @@ def suiteReadTests():
 
 
 if __name__ == "__main__":
-
     mySuite = suiteReadTests()
     unittest.TextTestRunner(verbosity=2).run(mySuite)
     #
