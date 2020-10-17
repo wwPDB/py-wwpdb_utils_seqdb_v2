8b7282fb92f21159346bff5c5768ec23c43317d3
Ezra Peisach <ezra.peisach@rcsb.org>
2020-10-16 20:04:54 -0400

    Get FetchUniProtEntryTests working through use of mock

--- wwpdb/utils/tests-seqdb/FetchUniProtEntryTests.py
+++ wwpdb/utils/tests-seqdb/FetchUniProtEntryTests.py
@@ -21,8 +21,21 @@ import os.path
 import string
 import traceback
 import platform
+from xml.dom import minidom
 
-from wwpdb.utils.config.ConfigInfo import getSiteId
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+import requests
+import requests_mock
+
+try:
+    from urllib.parse import parse_qs
+except ImportError:
+    from urlparse import parse_qs
+
+from wwpdb.utils.config.ConfigInfo import getSiteId, ConfigInfo
 from wwpdb.utils.seqdb_v2.FetchUniProtEntry import FetchUniProtEntry
 
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -32,7 +45,85 @@ if not os.path.exists(TESTOUTPUT):
     os.makedirs(TESTOUTPUT)
 
 
-@unittest.skip("Until have downloaded uniprot variant file")
+class MyConfigInfo(ConfigInfo):
+    """A class to bypass setting of refdata"""
+
+    def __init__(self, siteId=None, verbose=True, log=sys.stderr):
+        super(MyConfigInfo, self).__init__(siteId=siteId, verbose=verbose, log=log)
+
+    def get(self, keyWord, default=None):
+        if keyWord == "SITE_REFDATA_SEQUENCE_DB_PATH":
+            val = os.path.join(HERE, "refdata")
+        else:
+            sys.stderr.write("XXXXX Unknown site config fetching\n")
+            val = super(MyConfigInfo, self).get(keyWord=keyWord, default=default)
+        return val
+
+
+# Mock request handlers
+# dbfetch parses the request and returns data
+def dbfetchTextCallBack(request, context):
+    """Handles dbfetch requests"""
+    dat = parse_qs(request.body)
+    accs = dat["id"][0]
+
+    # Create a combined file of contents
+    node = None
+    root = None
+
+    for acc in accs.split(","):
+        fpath = os.path.join(HERE, "refdata", "dbfetch", acc + ".xml")
+        if not os.path.exists(fpath):
+            context.status_code = 404
+            print("XXX COULD NOT FIND", fpath)
+            return ""
+
+        doc = minidom.parse(fpath)
+        if not node:
+            node = doc
+            root = node.documentElement
+        else:
+            ent = doc.getElementsByTagName("entry")
+            for e in ent:
+                root.appendChild(e)
+
+    if node:
+        ret = root.toxml()
+    else:
+        ret = ""
+    return ret
+
+
+def isoformMatcher(request):
+    """Isoform search matcher of the form
+    https://www.ebi.ac.uk/proteins/api/proteins/P29994/isoforms
+    """
+    if "https://www.ebi.ac.uk/proteins/api" in request.url:
+        resp = requests.Response()
+
+        # Parse the request to get the id
+        preq = request.path_url.split("/")
+        if len(preq) < 6:
+            return None
+        iso = preq[5]
+        acc = preq[4]
+        if iso != "isoforms":
+            return None
+
+        fpath = os.path.join(HERE, "refdata", "ebi_proteins_isoforms", acc + ".xml")
+        if not os.path.exists(fpath):
+            print("XXX: File %s not found" % fpath)
+            resp.status_code = 404
+            return resp
+
+        resp.status_code = 200
+        with open(fpath, "rb") as fin:
+            resp._content = fin.read()  # pylint: disable=protected-access
+        return resp
+    # Error - not found
+    return None
+
+
 class FetchUniProtEntryTests(unittest.TestCase):
     def setUp(self):
         self.__verbose = True
@@ -42,14 +133,21 @@ class FetchUniProtEntryTests(unittest.TestCase):
         # self.__unpIdListV=['P42284','P42284-1','P42284-2','P42284-3','P29994-1','P29994-2','P29994-3','P29994-4','P29994-5','P29994-6','P29994-7','Q9NYG8-2']
         # self.__unpIdListV = ['P01116-2', 'P01116-2', 'Q9H8M2-1']
         self.__unpIdListV = ["Q9D6K9", "Q9D6K9-1", "Q9D6K9-2"]
-        #
+        if "MOCKREQUESTS" in os.environ:
+            self.__mock = requests_mock.Mocker()
+            self.__mock.post("https://www.ebi.ac.uk/Tools/dbfetch/dbfetch", text=dbfetchTextCallBack)
+            self.__mock.add_matcher(isoformMatcher)
+            self.__mock.start()
 
     def tearDown(self):
-        pass
+        if self.__mock is not None:
+            self.__mock.stop()
 
-    def testFetchVariantIds(self):
+    @patch("wwpdb.utils.seqdb_v2.FetchUniProtEntry.ConfigInfo", side_effect=MyConfigInfo)
+    def testFetchVariantIds(self, mock1):  # pylint: disable=unused-argument
         """"""
         self.__lfh.write("\nStarting FetchUniProtEntryTests testFetchVariantIds\n")
+        fobj = FetchUniProtEntry(siteId=self.__siteId, verbose=self.__verbose, log=self.__lfh)
         try:
             fobj = FetchUniProtEntry(siteId=self.__siteId, verbose=self.__verbose, log=self.__lfh)
             for unpid in self.__unpIdListV:
@@ -69,6 +167,7 @@ class FetchUniProtEntryTests(unittest.TestCase):
                             self.__lfh.write("------ No matching isoform for %s\n" % unpid)
                 else:
                     self.__lfh.write("+WARNING - Fetch failed for id %s\n" % unpid)
+                    self.fail()
         except:  # noqa: E722 pylint: disable=bare-except
             traceback.print_exc(file=self.__lfh)
             self.fail()
