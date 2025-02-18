32ed37e307f4a1c5360c644b31d591c9c3d6a501
Ezra Peisach <ezra.peisach@rcsb.org>
2025-02-17 23:05:09 -0500

    Remove hatchling

---
+++ setup.py
@@ -0,0 +1,65 @@
+# File: setup.py
+# Date: 3-Oct-2018
+#
+# Update:
+#
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+packages = []
+thisPackage = "wwpdb.utils.seqdb_v2"
+
+with open("wwpdb/utils/seqdb_v2/__init__.py", "r") as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError("Cannot find version information")
+
+setup(
+    name=thisPackage,
+    version=version,
+    description="wwPDB Sequence search utilities",
+    long_description="See:  README.md",
+    author="Ezra Peisach",
+    author_email="ezra.peisach@rcsb.org",
+    url="https://github.com/rcsb/py-wwpdb_utils_seqdb_v2",
+    #
+    license="Apache 2.0",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        # 'Development Status :: 5 - Production/Stable',
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    #
+    install_requires=["mmcif", "mmcif.utils", "wwpdb.utils.config ~= 0.33", "requests"],
+    packages=find_packages(exclude=["wwpdb.utils.tests-seqdb", "mock-data", "tests.*"]),
+    package_data={
+        # If any package contains *.md or *.rst ...  files, include them:
+        "": ["*.md", "*.rst", "*.txt", "*.cfg"],
+    },
+    #
+    # These basic tests require no database services -
+    test_suite="wwpdb.utils.tests-seqdb",
+    tests_require=["tox"],
+    #
+    # Not configured ...
+    extras_require={
+        "dev": ["check-manifest"],
+        "test": ["coverage"],
+    },
+    # Added for
+    command_options={"build_sphinx": {"project": ("setup.py", thisPackage), "version": ("setup.py", version), "release": ("setup.py", version)}},
+    # This setting for namespace package support -
+    zip_safe=False,
+)
